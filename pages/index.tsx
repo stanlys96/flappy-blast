9900574d2e045f73becfe930a29d4ac2fa769282
stanlys96 <stanlyskwok@gmail.com>
2024-06-07 20:24:45 +0700

    walo

--- pages/index.tsx
+++ pages/index.tsx
@@ -402,12 +402,12 @@ export default function HomePage() {
               </div>
             </div>
           </div>
-          <div className="large-desktop:mt-[100px] mt-[50px] md:mt-[25px] desktop:mt-[20px] flex flex-col p-[30px] large-desktop:p-[60px] gap-y-[20px] border border-[12px] border-[#404833] w-fit mx-auto border-dashed-custom justify-center items-center py-[36px]">
+          <div className="large-desktop:mt-[100px] mt-[50px] md:mt-[25px] desktop:mt-[20px] flex flex-col p-[20px] md:p-[30px] large-desktop:p-[60px] gap-y-[20px] border border-[12px] border-[#404833] w-fit mx-auto border-dashed-custom justify-center items-center py-[36px]">
             <p className="text-white text-[16px] md:text-[2rem] desktop:text-[2rem] large-desktop:text-[3rem]">
               in collaboration with
             </p>
             <a href="https://app.bladeswap.xyz/swap" target="_blank">
-              <BladeSvg className="h-[50px] cursor-pointer md:h-[75px] large-desktop:h-[120px] w-fit" />
+              <BladeSvg className="h-[45px] cursor-pointer md:h-[75px] large-desktop:h-[120px] w-fit" />
             </a>
           </div>
         </div>
