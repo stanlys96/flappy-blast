72a1c835a0af979980fc010e7c787e6a378f6e4c
stanlys96 <stanlyskwok@gmail.com>
2024-06-15 23:51:49 +0700

    Update to merge with main

--- src/layouts/HeroLayout.tsx
+++ src/layouts/HeroLayout.tsx
@@ -1,15 +1,22 @@
 import { AbsoluteImagesComponent } from "@/src/components/AbsoluteImagesComponent";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface Props {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 export const HeroLayout = ({ children }: Props) => {
-  return (
-    <div className="h-[120vh] md:h-[100vh] hero-section overflow-hidden w-[100vw] relative">
-      <AbsoluteImagesComponent />
-      {children}
-    </div>
-  );
+    const router = useRouter();
+    useEffect(() => {
+        if (router.pathname === "/airdrop") {
+            router.push("/");
+        }
+    }, []);
+    return (
+        <div className="h-[120vh] md:h-[100vh] hero-section overflow-hidden w-[100vw] relative">
+            <AbsoluteImagesComponent />
+            {children}
+        </div>
+    );
 };
