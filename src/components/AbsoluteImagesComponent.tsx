073ff735293da57802ab435a78eedb5a8c6669e4
stanlys96 <stanlyskwok@gmail.com>
2024-06-17 01:41:56 +0700

    disable airdrop

--- src/components/AbsoluteImagesComponent.tsx
+++ src/components/AbsoluteImagesComponent.tsx
@@ -74,7 +74,7 @@ export const AbsoluteImagesComponent = () => {
                         HOME
                     </a>
                     <a
-                        onClick={() => router.push("/airdrop")}
+                        onClick={() => {}}
                         className={`text-[20px] font-bold p-0 flex items-center text-center justify-center transition duration-500 hover:text-[#B0CD80] md:text-[1.5rem] desktop:text-[2.5rem] large-desktop:text-[3.5rem] cursor-pointer ${
                             router.pathname === "/airdrop"
                                 ? "text-[#FCFC03] hover:text-[#FCFC03]"
